0e6aacf07525f17aeb915c731309366f03eb01c6
“gravitybringsme” <gravitybringsme@gmail.com>
2024-06-24 00:25:46 +0900

    추가적 페이지 생성시 대응 코드 추가

--- app/src/main/java/com/neppplus/viewpagerpractice/MainActivity.kt
+++ app/src/main/java/com/neppplus/viewpagerpractice/MainActivity.kt
@@ -5,8 +5,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.neppplus.viewpagerpractice.adapters.MainViewPagerAdapter
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mViewPagerAdapter : MainViewPagerAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +19,13 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        mViewPagerAdapter = MainViewPagerAdapter(supportFragmentManager)
+
+        val mainViewPager = findViewById<androidx.viewpager.widget.ViewPager>(R.id.mainViewPager)
+        mainViewPager.adapter = mViewPagerAdapter
+        // 페이지가 많아지면 간단하게 부드럽도록 지원하는 기능
+        mainViewPager.offscreenPageLimit = 5
+
     }
 }
