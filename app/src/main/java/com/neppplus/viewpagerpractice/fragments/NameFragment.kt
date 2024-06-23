0d40a1dfb6cf9c6b0e1369ab0f69b1f741edda0d
“gravitybringsme” <gravitybringsme@gmail.com>
2024-06-24 00:08:50 +0900

    뷰페이저에 배치될 3개의 프래그먼트 완성

---
+++ app/src/main/java/com/neppplus/viewpagerpractice/fragments/NameFragment.kt
@@ -0,0 +1,24 @@
+package com.neppplus.viewpagerpractice.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.neppplus.viewpagerpractice.R
+
+class NameFragment:Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_name, container, false    )
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+
+}
