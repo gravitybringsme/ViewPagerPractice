2ec5a8cb4df69d6b3cc137910f7f78c6de12aff9
“gravitybringsme” <gravitybringsme@gmail.com>
2024-06-24 00:33:27 +0900

    페이지별 제목 설정

--- app/src/main/java/com/neppplus/viewpagerpractice/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/neppplus/viewpagerpractice/adapters/MainViewPagerAdapter.kt
@@ -8,6 +8,18 @@ import com.neppplus.viewpagerpractice.fragments.MyInfoFragment
 import com.neppplus.viewpagerpractice.fragments.NameFragment
 
 class MainViewPagerAdapter(fm : FragmentManager) :FragmentPagerAdapter(fm){
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        if(position == 0){
+            return "이름"
+        }
+        else if(position == 1){
+            return "내 정보"
+        }
+        else{
+            return "인사말"
+        }
+    }
     override fun getCount(): Int {
         return 3
     }
